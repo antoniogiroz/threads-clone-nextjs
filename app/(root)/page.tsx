d65a80011d763f4b93e9683f68f7c2c47b17c456
Antonio Giroz <antonioluisgil@gmail.com>
2023-08-05 16:36:51 +0200

    feat: add layout components

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -2,9 +2,8 @@ import { UserButton } from "@clerk/nextjs";
 
 export default function Home() {
   return (
-    <div>
-      <UserButton afterSignOutUrl="/" />
-      Home
-    </div>
+    <>
+      <h1 className="head-text text-left">Home</h1>
+    </>
   );
 }
